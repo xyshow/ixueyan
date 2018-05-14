12135edc5f6acadbdcdf0867fd91565f7ec6a2e1
xyshow <39022272+xyshow@users.noreply.github.com>
2018-05-14 20:32:23 +0800

    抢票--登陆状态
    在12306首页选好车次等信息
       * 选定以后在浏览器控制台输入此脚本
       * 脚本的作用是调刷新时间，也就是抢票频率
       * 抢到以后会直接调到付款页面
       * --完成
       *

---
+++ 12306.js
@@ -0,0 +1,15 @@
+
+  javascript:(function(an56_net){an56_net(an56_net);})(function(an56_net){if($('#tip:contains("恭喜")').is(':visible')) return false;else{$('#qd_closeDefaultWarningWindowDialog_id').click();$('#date_range li.sel').click();return setTimeout(function(){an56_net(an56_net);},2000);}});
+
+
+
+  /*
+   * 抢票--登陆状态
+   * 在12306首页选好车次等信息
+   * 选定以后在浏览器控制台输入此脚本
+   * 脚本的作用是调刷新时间，也就是抢票频率
+   * 抢到以后会直接调到付款页面
+   * --完成
+   *
+   */
+
